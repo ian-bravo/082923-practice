bc4757c6d941f9fd27216a5b8345c7c7fe668c22
ian-bravo <bravo.ian@gmail.com>
2023-08-29 13:53:02 -0700

    fail 'good' at test
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,11 +1,35 @@
-import triangleDeterminer from './../src/index.js';
+import Triangle from './../src/index.js';
 
-describe('triangleDeterminer', () => {
+describe('Triangle', () => {
+
+  let reusableTriangle;
+
+  beforeEach(() => {
+    reusableTriangle = new Triangle(5, 5, 5);
+  });
 
   test('will show proper import of function', () => {
-    const triangle = new triangleDeterminer(2,4,5);
-    expect(triangle.s1).toEqual(2);
-    expect(triangle.s2).toEqual(4);
-    expect(triangle.s3).toEqual(5);
+    expect(reusableTriangle.s1).toEqual(5);
+    expect(reusableTriangle.s2).toEqual(5);
+    expect(reusableTriangle.s3).toEqual(5);
+  });
+
+  test('will define what is a triangle', () => {
+    const triangle = new Triangle(2,4,5);
+    expect(triangle.s1 < (triangle.s2 + triangle.s3) && triangle.s3 < (triangle.s1 + triangle.s2) && triangle.s2 < (triangle.s1 + triangle.s3)).toEqual();
   });
 });
+
+//     if(s2 + s3 > s1 && s1 + s2 > s3 && s3 + s1 > s2) {
+//         if (s1 === s2 && s2 === s3 && s3 === s1) {
+//             response = "Equilateral triangle.";
+//         } else if (s1 === s2 || s2 === s3 || s3 === s1) {
+//             response = "Isosceles triangle.";
+//         } else {
+//             response = "Scalene triangle.";
+//         }
+//     } else {
+//         response = "Not a triangle.";
+//     }
+//     return response;
+// }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 
 
 
-export function triangleDeterminer(s1, s2, s3) {
-    let response;}
+export default function Triangle(s1, s2, s3) {
+    //let response;
+    this.s1 = s1;
+    this.s2 = s2;
+    this.s3 = s3;
+}
 //     if(s2 + s3 > s1 && s1 + s2 > s3 && s3 + s1 > s2) {
 //         if (s1 === s2 && s2 === s3 && s3 === s1) {
 //             response = "Equilateral triangle.";
